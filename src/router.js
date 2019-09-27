1d620d5bb5b1c1963a76adf919842e2604f4070d
Evgenii Kuznetsov <harsel1234@gmail.com>
2019-09-28 01:08:27 +0300

    добавлена структура страниц

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import { Home } from './views'
+import { Home, Cabinet, Login, Resume, NotFound } from './views'
 
 Vue.use(Router)
 
@@ -11,6 +11,22 @@ export default new Router({
     {
       path: '/',
       component: Home
+    },
+    {
+      path: '/cabinet',
+      component: Cabinet
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/resume/:id',
+      component: Resume
+    },
+    {
+      path: '*',
+      component: NotFound
     }
   ]
 })
--- src/views/index.js
+++ src/views/index.js
@@ -1,1 +1,5 @@
+export { default as Cabinet } from './Cabinet'
 export { default as Home } from './Home'
+export { default as Login } from './Login'
+export { default as Resume } from './Resume'
+export { default as NotFound } from './NotFound'
